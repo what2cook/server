44d4833e7f1e2bee8db5b443a638550f4f46c41b
방승연 <baaaang_53@yonsei.ac.kr>
2021-07-23 00:05:29 +0900

    Git init

---
+++ src/main/java/com/what2cook/what2cook/What2cookApplication.java
@@ -0,0 +1,13 @@
+package com.what2cook.what2cook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class What2cookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(What2cookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/what2cook/what2cook/What2cookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.what2cook.what2cook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class What2cookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
