16e8f21292ddd09ba858d2e8c91f967ef96e80a2
방승연 <baaaang_53@yonsei.ac.kr>
2021-09-05 14:18:55 +0900

    Develop @LoginUser annotation

---
+++ src/main/java/com/what2cook/what2cook/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.what2cook.what2cook.config;
+
+import com.what2cook.what2cook.config.auth.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+}
